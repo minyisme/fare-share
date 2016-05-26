44e3656b4bb812d11e2bba42fc3f5343576afe92
Min Yang <maggieyang@Mins-MacBook-Pro.local>
2016-05-26 16:19:31 -0700

    changed app to db setup

--- model.py
+++ model.py
@@ -218,7 +218,8 @@ def connect_to_db(app):
     """Connect the database to our Flask app"""
 
     # Configure to use our PostgreSQL database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://maggieyang:hackbright@localhost:5432/travels'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://maggieyang:hackbright@localhost:5432/travels'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///travels'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
